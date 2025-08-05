4ee5a6101b4d8d2129df293f3fd0c490ba29b90e
AstraVerse <astraverseapps@gmail.com>
2025-08-05 20:17:50 +0000

    Have you configured the database astryde-app-db In the app?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -17,7 +17,7 @@ const firebaseConfig = {
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
-const db = getFirestore(app);
+const db = getFirestore(app, process.env.NEXT_PUBLIC_FIREBASE_DATABASE_ID);
 const storage = getStorage(app);
 
 const analytics = (async () => {

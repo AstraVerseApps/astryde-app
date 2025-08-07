69d2ce8ce332894cf315fa2e1034a168980b6120
AstraVerse <astraverseapps@gmail.com>
2025-08-07 10:09:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,6 @@
 
+'use client';
+
 import { AstrydeLogo } from '@/components/icons';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
